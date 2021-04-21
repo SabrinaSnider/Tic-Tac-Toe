09f12243c58f7d0406f195ba87c6e11bfc661745
Sabrina Snider <sabrina.snider10@gmail.com>
2021-04-20 23:08:18 -0400

    UI developments for board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import logo from './logo.svg';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [size, setSize] = useState(3);
+  const [board, setBoard] = useState(Array.from(Array(3), () => Array(3).fill(undefined)));
+
+  const HUMAN_PIECE = "X";
+  const COMPUTER_PIECE = "O";
+
+  function getPositionClass(element) {
+    if (element === "X") return "X-position";
+    else if (element === "O") return "O-position";
+    else return "empty-position";
+  }
+
+  function selectPosition(row, column) {
+    let boardCopy = board.slice();
+    boardCopy[row][column] = HUMAN_PIECE;
+    setBoard(boardCopy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div id="app">
+      <div id="header">
+
+      </div>
+      <div id="game-container">
+        <Grid container direction="column">
+          {console.log(board)}
+          {board.map((row, rowNumber) =>
+            <Grid item key={rowNumber}>
+              {row.map((element, colNumber) => 
+                <Button
+                  variant="outlined"
+                  className={"grid-position " + getPositionClass(element)}
+                  disabled={element === HUMAN_PIECE || element === COMPUTER_PIECE}
+                  onClick={() => selectPosition(rowNumber, colNumber)}
+                  key={colNumber}
+                >
+                  {element}
+                </Button>
+              )}
+            </Grid>
+          )}
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/algorithm.js
+++ src/algorithm.js
@@ -11,96 +11,96 @@
 const BOARD_HEIGHT = 3;
 const MINMAX_DEPTH = 3;
 
-function move(board) {
-    const result = minmax(MINMAX_DEPTH, "COMPUTER", board);
-    board[result.bestMove[0]][result.bestMove[1]] = "COMPUTER";
-    return board;
+function move(board, HUMAN_PIECE, COMPUTER_PIECE) {
+  const result = minmax(MINMAX_DEPTH, COMPUTER_PIECE, board, HUMAN_PIECE, COMPUTER_PIECE);
+  board[result.bestMove[0]][result.bestMove[1]] = COMPUTER_PIECE;
+  return board;
 }
 
-function minmax(depth, player, board) {
-    const moves = potentialMoves(board);
-    let result = {
-        bestScore: undefined,
-        bestMove: undefined
-    }
+function minmax(depth, player, board, HUMAN_PIECE, COMPUTER_PIECE) {
+  const moves = potentialMoves(board);
+  let result = {
+    bestScore: undefined,
+    bestMove: undefined
+  }
 
-    if (!moves || depth == 0) {
-        result.bestScore = evaluate(board);
-        return result;
-    }
-
-    moves.forEach(move => {
-        board[move[0]][move[1]] = player;
-        let currentMove = minmax(depth - 1, player == "COMPUTER" ? "HUMAN" : "COMPUTER", board);
-
-        if (!result.bestScore ||
-            (player == "COMPUTER" && currentMove.bestScore > result.bestScore) ||
-            (player == "HUMAN" && currentMove.current < result.bestScore)) {
-                result.bestScore = currentMove.bestScore;
-                result.bestMove = move;
-        }
-        
-        board[move[0]][move[1]] = undefined;
-    })
+  if (!moves || depth === 0) {
+    result.bestScore = evaluate(board);
     return result;
+  }
+
+  moves.forEach(move => {
+    board[move[0]][move[1]] = player;
+    let currentMove = minmax(depth - 1, player === COMPUTER_PIECE ? HUMAN_PIECE : COMPUTER_PIECE, board);
+
+    if (!result.bestScore ||
+      (player === COMPUTER_PIECE && currentMove.bestScore > result.bestScore) ||
+      (player === HUMAN_PIECE && currentMove.current < result.bestScore)) {
+        result.bestScore = currentMove.bestScore;
+        result.bestMove = move;
+    }
+    
+    board[move[0]][move[1]] = undefined;
+  })
+  return result;
 }
 
 function potentialMoves(board) {
-    let moves = [];
-    for (let row = 0; row < BOARD_HEIGHT; row++) {
-        for (let col = 0; col < BOARD_HEIGHT; col++) {
-            if (!board[row][col]) moves.push([row, col])
-        }
-    }
-    return moves;
-}
-
-function evaluate(board) {
-    let score = 0;
-
-    // Vertical
+  let moves = [];
+  for (let row = 0; row < BOARD_HEIGHT; row++) {
     for (let col = 0; col < BOARD_HEIGHT; col++) {
-        positions = [];
-        for (let row = 0; row < BOARD_HEIGHT; row++) positions.push([row, col]);
-        score += evaluateLine(positions, board);
+      if (!board[row][col]) moves.push([row, col])
     }
-
-    // Horizontal
-    for (let row = 0; row < BOARD_HEIGHT; row++) {
-        positions = [];
-        for (let col = 0; col < BOARD_HEIGHT; col++) positions.push([row, col]);
-        score += evaluateLine(positions, board);
-    }
-
-    // Diagonal
-    positions = [];
-    for (let pos = 0; pos < BOARD_HEIGHT; pos++) positions.push([pos, pos]);
-    score += evaluateLine(positions, board);
-    
-    positions = [];
-    for (let pos = 0; pos < BOARD_HEIGHT; pos++) positions.push([pos, BOARD_HEIGHT - pos]);
-    score += evaluateLine(positions, board);
-
-    return score;
+  }
+  return moves;
 }
 
-function evaluateLine(positions, board) {
-    let humanCount = 0, computerCount = 0;
+function evaluate(board, HUMAN_PIECE, COMPUTER_PIECE) {
+  let score = 0;
 
-    positions.forEach(position => {
-        const player = board[position[0]][position[1]];
-        if (player == "COMPUTER") computerCount++; else if (player == "HUMAN") humanCount++;
-    });
-    
-    if (humanCount == 0) return 1 ** computerCount;
-    else if (computerCount == 0) return -1 * (1 ** humanCount);
-    else return 0;
+  // Vertical
+  for (let col = 0; col < BOARD_HEIGHT; col++) {
+    positions = [];
+    for (let row = 0; row < BOARD_HEIGHT; row++) positions.push([row, col]);
+    score += evaluateLine(positions, board, HUMAN_PIECE, COMPUTER_PIECE);
+  }
+
+  // Horizontal
+  for (let row = 0; row < BOARD_HEIGHT; row++) {
+    positions = [];
+    for (let col = 0; col < BOARD_HEIGHT; col++) positions.push([row, col]);
+    score += evaluateLine(positions, board, HUMAN_PIECE, COMPUTER_PIECE);
+  }
+
+  // Diagonal
+  positions = [];
+  for (let pos = 0; pos < BOARD_HEIGHT; pos++) positions.push([pos, pos]);
+  score += evaluateLine(positions, board, HUMAN_PIECE, COMPUTER_PIECE);
+  
+  positions = [];
+  for (let pos = 0; pos < BOARD_HEIGHT; pos++) positions.push([pos, BOARD_HEIGHT - pos]);
+  score += evaluateLine(positions, board, HUMAN_PIECE, COMPUTER_PIECE);
+
+  return score;
+}
+
+function evaluateLine(positions, board, HUMAN_PIECE, COMPUTER_PIECE) {
+  let humanCount = 0, computerCount = 0;
+
+  positions.forEach(position => {
+    const player = board[position[0]][position[1]];
+    if (player === COMPUTER_PIECE) computerCount++; else if (player === HUMAN_PIECE) humanCount++;
+  });
+  
+  if (humanCount == 0) return 1 ** computerCount;
+  else if (computerCount == 0) return -1 * (1 ** humanCount);
+  else return 0;
 }
 
 
 let board = Array.from(Array(3), () => Array(3).fill(undefined));
 console.log(board);
-board = move(board)
+board = move(board, "X", "O")
 console.log(board);
 // board[0][1] = "HUMAN";
 // console.log(board);
