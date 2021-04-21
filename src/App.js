d9a386e843eef593d782252860c2a3f16d1acb04
Sabrina Snider <sabrina.snider10@gmail.com>
2021-04-21 01:38:44 -0400

    Fixed win condition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import move from './algorithm';
 
 function App() {
@@ -15,36 +15,44 @@ function App() {
     Array.from(Array(boardHeight), () => Array(boardHeight).fill(undefined))
   );
 
+  useEffect(() => {
+    if (gameEnd) {
+      setTurn(undefined);
+      console.log("Game end!");
+    } else if (turn === COMPUTER_PIECE) {
+      computerMove();
+    }
+  }, [turn]);
+
   function getPositionClass(element) {
     if (element === "X") return "X-position";
     else if (element === "O") return "O-position";
     else return "empty-position";
   }
 
-  function selectPosition(row, column) {
+  function humanMove(row, column) {
     let boardCopy = board.slice();
     boardCopy[row][column] = HUMAN_PIECE;
     setBoard(boardCopy);
     
     checkIfEnd();
-    console.log("game ended?", gameEnd)
-    if (!gameEnd) setTimeout(() => computerMove(), 800);
+    if (!gameEnd) setTimeout(() => setTurn(COMPUTER_PIECE), 500);
   }
 
   function computerMove() {
-    setTurn(COMPUTER_PIECE);
-
     let boardCopy = board.slice();
     boardCopy = move(boardCopy, HUMAN_PIECE, COMPUTER_PIECE)
     setBoard(boardCopy);
 
     checkIfEnd();
-    console.log("game ended?", gameEnd)
-    if (!gameEnd) setTurn(HUMAN_PIECE);
+    if (!gameEnd) setTimeout(() => setTurn(HUMAN_PIECE), 500);
   }
 
   function checkIfEnd() {
-    // Row
+    if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) setGameEnd(true);
+  }
+
+  function checkHorizontalWin() {
     for (let row = 0; row < board[0].length; row++) {
       if (!board[row][0]) break;
       const player = board[row][0];
@@ -54,14 +62,12 @@ function App() {
         if (board[row][col] !== player) win = false;
       }
       
-      if (win) {
-        setGameEnd(true);
-        console.log("row win for", player, gameEnd)
-        return;
-      }
+      if (win) return true;
     }
+    return false;
+  }
 
-    // Column
+  function checkVerticalWin() {
     for (let col = 0; col < board.length; col++) {
       if (!board[0][col]) break;
       const player = board[0][col];
@@ -71,15 +77,35 @@ function App() {
         if (board[row][col] !== player) win = false;
       }
 
-      if (win) {
-        setGameEnd(true);
-        console.log("col win for", player, gameEnd)
-        return;
-      }
+      if (win) return true;
     }
+    return false;
+  }
 
-    // Vertical
-    
+  function checkDiagonalWin() {
+    // Diagonal starting from top left
+    let win = true;
+    let player = board[0][0];
+    if (player) {
+      for (let pos = 0; pos < board.length; pos++) {
+        if (board[pos][pos] !== player) win = false;
+      }
+    } else {
+      win = false;
+    }
+    if (win) return true;
+
+    // Diagonal starting from top right
+    win = true;
+    player = board[0][board.length - 1];
+    if (player) {
+      for (let pos = 0; pos < board.length; pos++) {
+        if (board[pos][board.length - pos - 1] !== player) win = false;
+      }
+    } else {
+      win = false;
+    }
+    return win;
   }
 
   return (
@@ -95,8 +121,8 @@ function App() {
                 <Button
                   variant="outlined"
                   className={"grid-position " + getPositionClass(element)}
-                  disabled={element === HUMAN_PIECE || element === COMPUTER_PIECE}
-                  onClick={() => selectPosition(rowNumber, colNumber)}
+                  disabled={element === HUMAN_PIECE || element === COMPUTER_PIECE || turn !== HUMAN_PIECE}
+                  onClick={() => humanMove(rowNumber, colNumber)}
                   key={colNumber}
                 >
                   {element}
